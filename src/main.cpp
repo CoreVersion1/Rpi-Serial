4132f0eb82335121e2571cc50ccdc484f55245ca
ZBY <17335777890@163.com>
2023-02-23 23:26:26 +0800

    optimize init gpio and return;

--- src/main.cpp
+++ src/main.cpp
@@ -6,39 +6,48 @@
 
 int main(int argc, char **argv)
 {
-  int gpio_cfg = gpioCfgGetInternals();
-  gpio_cfg |= PI_CFG_NOSIGHANDLER; // (1<<10)
-  gpioCfgSetInternals(gpio_cfg);
-  int status = gpioInitialise();
-  LOG(INFO) << "gpio init = " << status;
+  if (gpioInitialise() < 0)
+  {
+    LOG(INFO) << " gpio init failed.";
+  }
+  else
+  {
+    LOG(INFO) << " gpio init success.";
+  }
 
-  //此处是ttyUSB0，根据实际设备节点进行配置
-  int pHandle = serOpen((char*)"/dev/ttyUSB0",115200,0);
+  // 此处是ttyUSB0，根据实际设备节点进行配置
+  int pHandle = serOpen((char *)"/dev/ttyUSB0", 115200, 0);
   char buff[0xFF] = {0};
+
+  if (pHandle < 0)
+  {
+    LOG(ERROR) << " Open Serial failed.";
+    return -1;
+  }
 
   while (true)
   {
-    //Send data = 0x31
-    std::cout << "Write Byte state = " 
-              << serWriteByte(pHandle,0x31)
+    // Send data = 0x31
+    std::cout << " Write Byte state = "
+              << serWriteByte(pHandle, 0x31)
               << std::endl;
-    
-    //Receive data
+
+    // Receive data
     uint16_t dataNum = serDataAvailable(pHandle);
-    if(dataNum)
+    if (dataNum)
     {
-      std::cout << "Read Bytes Number = " 
-                << serRead(pHandle,buff,dataNum)
+      std::cout << " Read Bytes Number = "
+                << serRead(pHandle, buff, dataNum)
                 << std::endl;
 
       std::cout << "Recv Data : ";
-      for(uint16_t i =0;i<dataNum;i++)
+      for (uint16_t i = 0; i < dataNum; i++)
       {
         std::cout << buff[i];
       }
       std::cout << "\r\n";
     }
-    
+
     sleep(2);
   }
 
